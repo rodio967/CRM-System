5a711fdd1fe1ee81b4c9776e668612dd690ea097
rodion <rdr1218@mail.ru>
2026-05-25 23:35:17 +0700

    feature: add service

---
+++ src/main/java/com/shift/crm/service/SellerService.java
@@ -0,0 +1,82 @@
+package com.shift.crm.service;
+
+import com.shift.crm.dto.request.SellerCreateRequest;
+import com.shift.crm.dto.request.SellerUpdateRequest;
+import com.shift.crm.dto.response.SellerResponse;
+import com.shift.crm.exception.ResourceNotFoundException;
+import com.shift.crm.mapper.SellerMapper;
+import com.shift.crm.model.Seller;
+import com.shift.crm.repository.SellerRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class SellerService {
+
+    private final SellerRepository sellerRepository;
+    private final SellerMapper sellerMapper;
+
+    @Transactional(readOnly = true)
+    public List<SellerResponse> findAll() {
+        return sellerRepository.findAllByDeletedFalse().stream()
+                .map(sellerMapper::toResponse)
+                .toList();
+    }
+
+    @Transactional(readOnly = true)
+    public SellerResponse findById(Long id) {
+        return sellerMapper.toResponse(getActiveSellerOrThrow(id));
+    }
+
+
+    public SellerResponse create(SellerCreateRequest request) {
+        Seller seller = new Seller(request.name(),
+                request.contactInfo(),
+                LocalDateTime.now()
+        );
+
+        Seller saved = sellerRepository.save(seller);
+        return sellerMapper.toResponse(saved);
+    }
+
+
+    public SellerResponse update(Long id, SellerUpdateRequest request) {
+        Seller seller = getActiveSellerOrThrow(id);
+
+        if (request.name() != null) seller.setName(request.name());
+        if (request.contactInfo() != null) seller.setContactInfo(request.contactInfo());
+
+        Seller saved = sellerRepository.save(seller);
+        return sellerMapper.toResponse(saved);
+    }
+
+    // soft delete
+    @Transactional
+    public void delete(Long id) {
+        Seller seller = getActiveSellerOrThrow(id);
+        seller.markDeleted();
+    }
+
+
+
+    @Transactional(readOnly = true)
+    public Seller getActiveSellerOrThrow(Long id) {
+        return sellerRepository.findByIdAndDeletedFalse(id)
+                .orElseThrow(() -> ResourceNotFoundException.seller(id));
+    }
+
+    @Transactional(readOnly = true)
+    public Seller getSellerOrTrow(Long id) {
+        return sellerRepository.findById(id)
+                .orElseThrow(() -> ResourceNotFoundException.seller(id));
+    }
+
+
+
+}
---
+++ src/main/java/com/shift/crm/service/TransactionService.java
@@ -0,0 +1,69 @@
+package com.shift.crm.service;
+
+import com.shift.crm.dto.request.TransactionCreateRequest;
+import com.shift.crm.dto.response.TransactionResponse;
+import com.shift.crm.exception.ResourceNotFoundException;
+import com.shift.crm.mapper.TransactionMapper;
+import com.shift.crm.model.Seller;
+import com.shift.crm.model.Transaction;
+import com.shift.crm.repository.TransactionRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class TransactionService {
+
+    private final TransactionRepository transactionalRepository;
+    private final SellerService sellerService;
+    private final TransactionMapper transactionMapper;
+
+    @Transactional(readOnly = true)
+    public List<TransactionResponse> findAll() {
+        return transactionalRepository.findAll().stream()
+                .map(transactionMapper::toResponse)
+                .toList();
+    }
+
+    @Transactional(readOnly = true)
+    public TransactionResponse findById(Long id) {
+        Transaction transaction = transactionalRepository.findById(id)
+                .orElseThrow(() -> ResourceNotFoundException.transaction(id));
+
+        return transactionMapper.toResponse(transaction);
+    }
+
+    @Transactional(readOnly = true)
+    public List<TransactionResponse> findBySellerId(Long sellerId) {
+        sellerService.getSellerOrTrow(sellerId);
+
+        return transactionalRepository.findAllBySellerIdOrderByTransactionDateAsc(sellerId)
+                .stream().
+                map(transactionMapper::toResponse)
+                .toList();
+    }
+
+    public TransactionResponse create(TransactionCreateRequest request) {
+        Seller seller = sellerService.getActiveSellerOrThrow(request.sellerId());
+        LocalDateTime date = request.transactionDate() != null
+                ? request.transactionDate()
+                : LocalDateTime.now();
+
+        Transaction transaction = new Transaction(
+                seller,
+                request.amount(),
+                request.paymentType(),
+                date
+                );
+
+        Transaction saved = transactionalRepository.save(transaction);
+        return transactionMapper.toResponse(saved);
+    }
+
+
+}
